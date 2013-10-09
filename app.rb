01a20fcf4e412b3cf8592fa43e84b8b59779f6f1
alex@laptop <karasev.alexey@gmail.com>
2013-10-10 00:59:32 +0400

    daemonized

--- app.rb
+++ app.rb
@@ -4,10 +4,12 @@ require_relative 'lib/cookie'
 require_relative 'lib/parse'
 
 def request_and_parse
-  doc=RestClient.get "https://direct.yandex.ru/registered/main.pl?cmd=showCamp&cid=7634904&ulogin=", :cookies => Cookie.FromFile('cookie.txt')
+  doc=RestClient.get "https://direct.yandex.ru/registered/main.pl?cmd=showCamp&cid=7634904&ulogin=", :cookies => Cookie.FromFile(File.expand_path(File.join(File.dirname(__FILE__), "cookie.txt")))
   result=Parse.CampaignParams(doc)
-  File.open('stats.csv', 'a:windows-1251') { |f| f.write(result) }
+  File.open(File.join(File.dirname(__FILE__), "stats.csv"), 'a:windows-1251') { |f| f.write(result) }
 end
 
-# puts Api.CreateNewForecast ["Говорящий Хомяк", "Хомяк Повторюшка"], ["213"]
-request_and_parse
+loop do
+  request_and_parse
+  sleep(50+rand(20))
+end
