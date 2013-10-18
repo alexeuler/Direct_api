d900e2a4f47762908a3b821ba23283eb8095dcf2
Alexey Karasev <alex@homa-online.ru>
2013-10-18 12:00:31 +0000

    added daemon monitor

--- server.rb
+++ server.rb
@@ -1,3 +1,10 @@
 require 'daemons'
 
-Daemons.run('app.rb')
+Daemons.run('app.rb', {
+              app_name: 'stats',
+              dir_mode: :script,
+              dir: 'daemon',
+              backtrace: true,
+              monitor: true,
+              log_output: true
+            })
